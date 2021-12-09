542df0d863e9d72c4865033f203c1f881ba47442
Alex <pf775_ash@rambler.ru>
2021-12-09 23:38:59 +0300

    VM

---
+++ MainWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Home13
+{
+    public partial class MainWindow : Window
+    {
+        private readonly MainWindowVM mainWindowVM;
+
+        public MainWindow(MainWindowVM mainWindowVM)
+        {
+            InitializeComponent();
+            this.mainWindowVM = mainWindowVM;
+            DataContext = mainWindowVM;
+        }
+    }
+}
