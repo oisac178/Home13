9922c3d751e71f82aef4325f32c23bb97c64385d
Alex <pf775_ash@rambler.ru>
2022-02-10 22:42:01 +0300

    checkboxconv

---
+++ CheckBoxConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Home13
+{
+    public class CheckBoxConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool boolean ? !boolean : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool boolean ? !boolean : value;
+        }
+    }
+}
