491239e03dfd7fce159679fe4f1defba70b8cba0
Alex <pf775_ash@rambler.ru>
2022-04-07 20:22:17 +0300

    wind

--- SomethingException.cs
+++ SomethingException.cs
@@ -9,11 +9,9 @@ namespace Home13
     class SomethingException : Exception
     {
         private Window_Acc_VM window_Acc_VM;
-        private MainWindowVM mainWindowVM;
         public string SomethingMessage { get; set; }
         public SomethingException(string Msg)
         {
-            window_Acc_VM = new Window_Acc_VM(mainWindowVM);
             if (window_Acc_VM.Sum > 9999999999999999 || window_Acc_VM.Sum < 1000000000000000)
             {
                 this.SomethingMessage = Msg;
