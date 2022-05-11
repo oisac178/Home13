3e75c7dc58d359e85e9e96899e53e8de4549180c
Alex <pf775_ash@rambler.ru>
2022-05-11 19:18:45 +0300

    ready

--- SaveData.cs
+++ SaveData.cs
@@ -67,34 +67,21 @@ namespace Home13
             Task<List<Client>> task5 = Task<List<Client>>.Factory.StartNew(TaskMethod, @"clients5.mydata");
             Task<List<Client>> task6 = Task<List<Client>>.Factory.StartNew(TaskMethod, @"clients6.mydata");
             Task<List<Client>> task7 = Task<List<Client>>.Factory.StartNew(TaskMethod, @"clients7.mydata");
-            lock (obj)
-            {
-                people.AddRange(task1.Result);
-            }
-            lock (obj)
-            {
-                people.AddRange(task2.Result);
-            }
-            lock (obj)
-            {
-                people.AddRange(task3.Result);
-            }
-            lock (obj)
-            {
-                people.AddRange(task4.Result);
-            }
-            lock (obj)
-            {
-                people.AddRange(task5.Result);
-            }
-            lock (obj)
-            {
-                people.AddRange(task6.Result);
-            }
-            lock (obj)
-            {
-                people.AddRange(task7.Result);
-            }
+            task1.Wait();
+            people.AddRange(task1.Result);
+            task2.Wait();
+            people.AddRange(task2.Result);
+            task3.Wait();
+            people.AddRange(task3.Result);
+            task4.Wait();
+            people.AddRange(task4.Result);
+            task5.Wait();
+            people.AddRange(task5.Result);
+            task6.Wait();
+            people.AddRange(task6.Result);
+            task7.Wait();
+            people.AddRange(task7.Result);
+            
             task1.Dispose();
             task2.Dispose();
             task3.Dispose();
